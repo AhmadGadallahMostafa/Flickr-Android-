e88df79f4baff4421f884589998e49f643bcbf34
Ameer Ragab <ameerragab1@yahoo.com>
2021-05-18 22:54:27 +0200

    final editing

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,29 +2,15 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroupOverlay;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
-
-import static android.app.Activity.RESULT_OK;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private static final int Request_Img_View=101 ;
+    private static final int Request_Img_View=101 ;  // da hena netsaraf feeh ezay
 
 
     @Override
@@ -34,27 +20,23 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.Imageview);
 
     }
-    public void camera(View view)
-    {
 
-        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(imageTakeIntent.resolveActivity(getPackageManager())!=null)
-        {
-            startActivityForResult(imageTakeIntent,Request_Img_View);
-        }
-    }
     @Override
     protected void onActivityResult(int RequestCode,int resultCode,Intent data) {
         super.onActivityResult(RequestCode, resultCode, data);
-        if (RequestCode == Request_Img_View && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
+
+        Camera.onCameraResult(RequestCode, resultCode, data, imageView);
     }
 
+    public void onClickCamera(View view)
+    {
+         Camera.onClickCamera(view, getPackageManager(),this);
+    }
+
+
+
     public void onButtonShowPopupWindowClick(View view) {
-        startActivity(new Intent(this, PopupActivity.class));
+        startActivity(new Intent(this, NotificationsActivity.class));
     }
     public void showAbout(View view) {
         startActivity(new Intent(this, AboutActivity.class));
--- app/src/main/java/com/example/justjava/NotificationsActivity.java
+++ app/src/main/java/com/example/justjava/NotificationsActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-public class PopupActivity extends Activity {
+public class NotificationsActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
