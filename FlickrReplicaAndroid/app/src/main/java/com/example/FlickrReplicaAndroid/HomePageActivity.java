e17cc4790209540f1c30838435db47a813bb05af
TheAX2 <ahmedase2000@gmail.com>
2021-06-03 14:16:17 +0200

    Renamed Proj

---
+++ FlickrReplicaAndroid/app/src/main/java/com/example/FlickrReplicaAndroid/HomePageActivity.java
@@ -0,0 +1,83 @@
+package com.example.FlickrReplicaAndroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout ;
+    private ViewPager viewPager2;
+    private MyPagerAdapter pagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        tabLayout = findViewById(R.id.homePageTabLayout);
+        viewPager2 = findViewById(R.id.homePageViewPager);
+        pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager2.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager2);
+        InitTabLayoutTabs();
+
+    }
+
+    /**
+     * Intializes the icons for the tablayout in the homepage
+     */
+    void InitTabLayoutTabs(){
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_feed);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_search);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_profile);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_notification_bell);
+        tabLayout.getTabAt(4).setIcon(R.drawable.ic_camera);
+
+    }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+
+        public MyPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Loads in the fragment that is selected by the tab layout of the viewpager depending on
+         * the position
+         * @param position the position of the selected fragement in the tab layout
+         * @return the fragment that should be selected
+         */
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+
+                case 0: return new HomeFeedFragment();
+                case 1: return new SearchFragment();
+                case 2: return new SearchFragment();
+                case 3: return new SearchFragment();
+                case 4: return new SearchFragment();
+                default: return new HomeFeedFragment();
+            }
+
+        }
+
+        /**
+         * gets the number of tabs in this tablayout
+         * @return the number of tabs in this tablayout
+         */
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
---
+++ FlickrReplicaAndroid/app/src/main/java/com/example/FlickrReplicaAndroid/UserProfile.java
@@ -0,0 +1,31 @@
+package com.example.FlickrReplicaAndroid;
+
+public class UserProfile {
+    private String name;
+    private String profilePicURL;
+    private boolean isPro;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfilePicURL(String profilePicURL) {
+        this.profilePicURL = profilePicURL;
+    }
+
+    public String getProfilePicURL() {
+        return profilePicURL;
+    }
+
+    public void setPro(boolean pro) {
+        isPro = pro;
+    }
+
+    public boolean isPro() {
+        return isPro;
+    }
+}
